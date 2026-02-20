f7dc64184d1341bafd57fdfdee8a3945a51dd11e
Jupazumur <a223201410@unison.mx>
2026-02-19 17:15:55 -0700

    Avance prueba_BA 2

--- prueba_BA.py
+++ prueba_BA.py
@@ -43,13 +43,13 @@ datos_entrenamiento = datos[:N]
 datos_validacion = datos[N:]
 
 # Para bosque
-clases = Counter(d[target] for d in datos)
+clases = Counter(d[target] for d in datos_entrenamiento)
 clase_default = clases.most_common(1)[0][0]
-M = 10
-variables_seleccionadas = int(np.sqrt(len(atributos)-1))
+#M = 10
+vars_optimas = int(np.sqrt(len(atributos)-1))
 
-# Para diferentes profundidades
-errores = []
+### Para diferentes profundidades ###
+errores_d = []
 for profundidad in [1, 3, 5, 10, 15, 20, 30]:
     bosque = ba.entrenar_bosque(
         datos_entrenamiento, 
@@ -58,26 +58,99 @@ for profundidad in [1, 3, 5, 10, 15, 20, 30]:
         max_profundidad=profundidad,
         acc_nodo=1.0,
         min_ejemplos=0,
-        M=M,
-        variables_seleccionadas=variables_seleccionadas
+        M=10,
+        variables_seleccionadas=vars_optimas
     )
 
     predicciones_in = ba.predecir_bosque(bosque, datos_entrenamiento)
-    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_validacion) if p == d[target])
+    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_entrenamiento) if p == d[target])
     e_in = 1.0 - (aciertos_in / len(datos_entrenamiento))
 
     predicciones_out = ba.predecir_bosque(bosque, datos_validacion)
     aciertos_out = sum(1 for p, d in zip(predicciones_out, datos_validacion) if p == d[target])
     e_out = 1.0 - (aciertos_out / len(datos_validacion))
 
-    errores.append( (profundidad, e_in, e_out) )
+    errores_d.append( (profundidad, e_in, e_out) )
     
-# Muestra los errores
-print('d'.center(10) + 'Ein'.center(15) + 'E_out'.center(15))
+# Erorres
+print("Errores a diferentes profundidades\n")
+print('d'.center(10) + 'E_in'.center(15) + 'E_out'.center(15))
 print('-' * 40)
-for profundidad, error_entrenamiento, error_validacion in errores:
+for profundidad, error_entrenamiento, error_validacion in errores_d:
     print(
         f'{profundidad}'.center(10) 
+        + f'{error_entrenamiento:.2f}'.center(15) 
+        + f'{error_validacion:.2f}'.center(15)
+    )
+print('-' * 40 + '\n')
+
+### Para diferentes cantidades de arboles (M) ###
+errores_M = []
+for M in [1, 5, 10, 20, 30, 40, 50]:
+    bosque = ba.entrenar_bosque(
+        datos_entrenamiento, 
+        target,
+        clase_default,
+        max_profundidad=None,
+        acc_nodo=1.0,
+        min_ejemplos=0,
+        M=M,
+        variables_seleccionadas=vars_optimas
+    )
+
+    predicciones_in = ba.predecir_bosque(bosque, datos_entrenamiento)
+    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_entrenamiento) if p == d[target])
+    e_in = 1.0 - (aciertos_in / len(datos_entrenamiento))
+
+    predicciones_out = ba.predecir_bosque(bosque, datos_validacion)
+    aciertos_out = sum(1 for p, d in zip(predicciones_out, datos_validacion) if p == d[target])
+    e_out = 1.0 - (aciertos_out / len(datos_validacion))
+
+    errores_M.append( (M, e_in, e_out) )
+
+## Errores
+print("Errores a diferentes cantidades de arboles (M)\n")
+print('M'.center(10) + 'E_in'.center(15) + 'E_out'.center(15))
+print('-' * 40)
+for M, error_entrenamiento, error_validacion in errores_M:
+    print(
+        f'{M}'.center(10) 
+        + f'{error_entrenamiento:.2f}'.center(15) 
+        + f'{error_validacion:.2f}'.center(15)
+    )
+print('-' * 40 + '\n')
+
+### Para diferentes cantidades de variables seleccionadas ###
+print("Errores a diferentes cantidades de variables seleccionadas\n")
+errores_var = []
+for vs in [2, 4, 8, 12, 16, 20, 24]:
+    bosque = ba.entrenar_bosque(
+        datos_entrenamiento, 
+        target,
+        clase_default,
+        max_profundidad=None,
+        acc_nodo=1.0,
+        min_ejemplos=0,
+        M=10,
+        variables_seleccionadas=vs
+    )
+
+    predicciones_in = ba.predecir_bosque(bosque, datos_entrenamiento)
+    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_entrenamiento) if p == d[target])
+    e_in = 1.0 - (aciertos_in / len(datos_entrenamiento))
+
+    predicciones_out = ba.predecir_bosque(bosque, datos_validacion)
+    aciertos_out = sum(1 for p, d in zip(predicciones_out, datos_validacion) if p == d[target])
+    e_out = 1.0 - (aciertos_out / len(datos_validacion))
+
+    errores_var.append( (vs, e_in, e_out) )
+
+# Errores
+print('vars'.center(10) + 'E_in'.center(15) + 'E_out'.center(15))
+print('-' * 40)
+for vs, error_entrenamiento, error_validacion in errores_var:
+    print(
+        f'{vs}'.center(10) 
         + f'{error_entrenamiento:.2f}'.center(15) 
         + f'{error_validacion:.2f}'.center(15)
     )
