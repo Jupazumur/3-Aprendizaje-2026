83023d647b156c2c80f31ccaf9e7cef724d91dfa
Jupazumur <a223201410@unison.mx>
2026-02-22 14:07:46 -0700

    Optimizacion bosque_aleatorio y refactor prueba_BA

--- bosque_aleatorio.py
+++ bosque_aleatorio.py
@@ -1,45 +1,77 @@
 import arboles_numericos as an
 import random
+from collections import Counter
 
 def entrenar_bosque(datos, target, clase_default, max_profundidad, acc_nodo, min_ejemplos, M, variables_seleccionadas):
-    bosque = []
-    muestras = __entrenar_bosque_helper(datos, M)
+    """
+    Entrena un bosque de M árboles, cada árbol entrenado con
+    un subconjunto de datos
+    """
+    #bosque = []
+    muestras = _bootstrap(datos, M)
 
-    for muestra in muestras:
-        arbol = an.entrena_arbol(
-            muestra,
-            target,
-            clase_default,
-            max_profundidad=max_profundidad,
-            acc_nodo=acc_nodo,
-            min_ejemplos=min_ejemplos,
-            variables_seleccionadas=variables_seleccionadas,
-        )
-        bosque.append(arbol)
+    #for muestra in muestras:
+    #    arbol = an.entrena_arbol(
+    #        muestra,
+    #        target,
+    #        clase_default,
+    #        max_profundidad=max_profundidad,
+    #        acc_nodo=acc_nodo,
+    #        min_ejemplos=min_ejemplos,
+    #        variables_seleccionadas=variables_seleccionadas,
+    #    )
+    #    bosque.append(arbol)
+
+    bosque = [an.entrena_arbol(
+                muestra,
+                target,
+                clase_default,
+                max_profundidad=max_profundidad,
+                acc_nodo=acc_nodo,
+                min_ejemplos=min_ejemplos,
+                variables_seleccionadas=variables_seleccionadas)
+            
+            for muestra in muestras]
 
     return bosque
 
-def __entrenar_bosque_helper(datos, M):
+def _bootstrap(datos, M):
     """
-    Bootstrapping
+    Realiza el proceso de Bootstrapping en conjunto de datos
     """
-    muestras = []
+    #muestras = []
+    k=len(datos)
 
-    for _ in range(M):
-        muestras.append(random.choices(datos, k=len(datos)))
+    #for _ in range(M):
+    #    muestras.append(random.choices(datos, k=k))
 
-    return muestras
+    #return muestras
+    return [random.choices(datos, k=k) for _ in range(M)]
 
 def predecir_bosque(bosque, datos):
-    predicciones = []
+    """
+    Reúne las predicciones de un bosque
+    """
+    #predicciones = []
 
-    for instancia in datos:
-        votos = []
+    #for instancia in datos:
+        #votos = []
 
-        for arbol in bosque:
-            votos.append(arbol.predice(instancia))
+        #for arbol in bosque:
+        #    votos.append(arbol.predice(instancia))
+
+        #votos = [arbol.predice(instancia) for arbol in bosque]
         
-        ganador = max(votos, key=votos.count)
-        predicciones.append(ganador)
+        #ganador = max(votos, key=votos.count)
+        #predicciones.append(ganador)
 
-    return predicciones
+    #return predicciones
+
+    return [_obtener_ganador(instancia, bosque) for instancia in datos]
+
+def _obtener_ganador(instancia, bosque):
+    """
+    Obtiene la clase mayoritaria del bosque para una instancia
+    """
+    votos = [arbol.predice(instancia) for arbol in bosque]
+    return Counter(votos).most_common(1)[0][0]
--- prueba_BA.py
+++ prueba_BA.py
@@ -1,133 +1,142 @@
 import utileria as ut
 import bosque_aleatorio as ba
-import numpy as np
+from numpy import sqrt
 from collections import Counter
 import os
-import random
+from random import seed, shuffle
+from time import perf_counter
 
-# Descarga y descomprime los datos
-url = "https://archive.ics.uci.edu/static/public/697/predict+students+dropout+and+academic+success.zip"
-archivo = "datos/dropout.zip"
-archivo_datos = "datos/data.csv"
-atributos = [f'feature_{i}' for i in range(1, 37)] + ['Target']
+def main():
 
-# Descarga datos
-if not os.path.exists("datos"):
-    os.makedirs("datos")
-if not os.path.exists(archivo):
-    ut.descarga_datos(url, archivo)
-    ut.descomprime_zip(archivo)
+    # Timer
+    t_comienzo = perf_counter()
 
-#Extrae datos y convierte a numericos
-datos = ut.lee_csv(
-    archivo_datos,
-    atributos=atributos,
-    separador=";"
-)
+    # Descarga y descomprime los datos
+    url = "https://archive.ics.uci.edu/static/public/697/predict+students+dropout+and+academic+success.zip"
+    archivo = "datos/dropout.zip"
+    archivo_datos = "datos/data.csv"
+    atributos = [f'feature_{i}' for i in range(1, 37)] + ['Target']
 
-for d in datos:
-    for i in range(1, 37):
-        d[f'feature_{i}'] = float(d[f'feature_{i}'])
+    # Descarga datos
+    if not os.path.exists("datos"):
+        os.makedirs("datos")
+    if not os.path.exists(archivo):
+        ut.descarga_datos(url, archivo)
+        ut.descomprime_zip(archivo)
 
-# Selecciona los artributos
-target = 'Target'
-atributos = [target] + [f'feature_{i}' for i in range(1, 37)]
+    #Extrae datos y convierte a numericos
+    datos = ut.lee_csv(
+        archivo_datos,
+        atributos=atributos,
+        separador=";"
+    )
 
-# Selecciona un conjunto de entrenamiento y de validación
-random.seed(42)
-random.shuffle(datos)
+    for d in datos:
+        for i in range(1, 37):
+            d[f'feature_{i}'] = float(d[f'feature_{i}'])
 
-# Truncamiento para pruebas más rápidas
-datos = datos[:600]
+    # Selecciona los artributos
+    target = 'Target'
+    atributos = [target] + [f'feature_{i}' for i in range(1, 37)]
 
-N = int(0.8*len(datos))
-datos_entrenamiento = datos[:N]
-datos_validacion = datos[N:]
+    # Selecciona un conjunto de entrenamiento y de validación
+    seed(42)
+    shuffle(datos)
 
-# Para bosque
-clases = Counter(d[target] for d in datos_entrenamiento)
-clase_default = clases.most_common(1)[0][0]
-#M = 10
-vars_optimas = int(np.sqrt(len(atributos)-1))
+    # Truncamiento para pruebas más rápidas
+    datos = datos[:100]
 
-### Para diferentes profundidades ###
-errores_d = []
-for profundidad in [1, 3, 5, 10, 15, 20, 30]:
+    # Split de datos de entrenamiento y validación
+    N = int(0.8*len(datos))
+    datos_entrenamiento = datos[:N]
+    datos_validacion = datos[N:]
+
+    # Para bosque
+    clases = Counter(d[target] for d in datos_entrenamiento)
+    clase_default = clases.most_common(1)[0][0]
+    vars_optimas = int(sqrt(len(atributos)-1))
+
+    errores_d = [test_profundidad(datos_entrenamiento, datos_validacion, target, clase_default, profundidad, vars_optimas)
+                 for profundidad in [1, 3, 5, 10, 15, 20, 30]]
+    
+    errores_M = [test_M(datos_entrenamiento, datos_validacion, target, clase_default, num_arboles, vars_optimas)
+                 for num_arboles in [1, 5, 10, 20, 30, 40, 50]]
+    
+    errores_var = [test_vs(datos_entrenamiento, datos_validacion, target, clase_default, num_vars)
+                 for num_vars in [2, 4, 8, 12, 16, 20, 24]]
+
+    print("Errores para diferentes profundidades\n")
+    imprimir_error(errores_d, 'd')
+
+    print("Errores para diferentes cantidades de arboles (M)\n")
+    imprimir_error(errores_M, 'M')
+
+    print("Errores para diferentes cantidades de variables seleccionadas\n")
+    imprimir_error(errores_var, 'vars')
+
+    t_final = perf_counter()
+
+    print(f"Tiempo ejecución: {(t_final - t_comienzo):.4f} seconds")
+
+def imprimir_error(errores, aspecto):
+    print(aspecto.center(10) + 'E_in'.center(15) + 'E_out'.center(15))
+    print('-' * 40)
+
+    for medicion, error_entrenamiento, error_validacion in errores:
+        print(
+            f'{medicion}'.center(10) 
+            + f'{error_entrenamiento:.2f}'.center(15) 
+            + f'{error_validacion:.2f}'.center(15)
+        )
+    print('-' * 40 + '\n')
+
+def test_profundidad(datos_e, datos_v, target, clase_default, max_profundidad, variables_seleccionadas):
     bosque = ba.entrenar_bosque(
-        datos_entrenamiento, 
+        datos_e, 
         target,
         clase_default,
-        max_profundidad=profundidad,
+        max_profundidad=max_profundidad,
         acc_nodo=1.0,
         min_ejemplos=0,
         M=10,
-        variables_seleccionadas=vars_optimas
+        variables_seleccionadas=variables_seleccionadas
     )
 
-    predicciones_in = ba.predecir_bosque(bosque, datos_entrenamiento)
-    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_entrenamiento) if p == d[target])
-    e_in = 1.0 - (aciertos_in / len(datos_entrenamiento))
+    predicciones_in = ba.predecir_bosque(bosque, datos_e)
+    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_e) if p == d[target])
+    e_in = 1.0 - (aciertos_in / len(datos_e))
 
-    predicciones_out = ba.predecir_bosque(bosque, datos_validacion)
-    aciertos_out = sum(1 for p, d in zip(predicciones_out, datos_validacion) if p == d[target])
-    e_out = 1.0 - (aciertos_out / len(datos_validacion))
+    predicciones_out = ba.predecir_bosque(bosque, datos_v)
+    aciertos_out = sum(1 for p, d in zip(predicciones_out, datos_v) if p == d[target])
+    e_out = 1.0 - (aciertos_out / len(datos_v))
 
-    errores_d.append( (profundidad, e_in, e_out) )
-    
-# Erorres
-print("Errores a diferentes profundidades\n")
-print('d'.center(10) + 'E_in'.center(15) + 'E_out'.center(15))
-print('-' * 40)
-for profundidad, error_entrenamiento, error_validacion in errores_d:
-    print(
-        f'{profundidad}'.center(10) 
-        + f'{error_entrenamiento:.2f}'.center(15) 
-        + f'{error_validacion:.2f}'.center(15)
-    )
-print('-' * 40 + '\n')
+    return (max_profundidad, e_in, e_out)
 
-### Para diferentes cantidades de arboles (M) ###
-errores_M = []
-for M in [1, 5, 10, 20, 30, 40, 50]:
+def test_M(datos_e, datos_v, target, clase_default, M, variables_seleccionadas):
     bosque = ba.entrenar_bosque(
-        datos_entrenamiento, 
+        datos_e, 
         target,
         clase_default,
         max_profundidad=None,
         acc_nodo=1.0,
         min_ejemplos=0,
         M=M,
-        variables_seleccionadas=vars_optimas
+        variables_seleccionadas=variables_seleccionadas
     )
 
-    predicciones_in = ba.predecir_bosque(bosque, datos_entrenamiento)
-    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_entrenamiento) if p == d[target])
-    e_in = 1.0 - (aciertos_in / len(datos_entrenamiento))
+    predicciones_in = ba.predecir_bosque(bosque, datos_e)
+    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_e) if p == d[target])
+    e_in = 1.0 - (aciertos_in / len(datos_e))
 
-    predicciones_out = ba.predecir_bosque(bosque, datos_validacion)
-    aciertos_out = sum(1 for p, d in zip(predicciones_out, datos_validacion) if p == d[target])
-    e_out = 1.0 - (aciertos_out / len(datos_validacion))
+    predicciones_out = ba.predecir_bosque(bosque, datos_v)
+    aciertos_out = sum(1 for p, d in zip(predicciones_out, datos_v) if p == d[target])
+    e_out = 1.0 - (aciertos_out / len(datos_v))
 
-    errores_M.append( (M, e_in, e_out) )
+    return (M, e_in, e_out)
 
-## Errores
-print("Errores a diferentes cantidades de arboles (M)\n")
-print('M'.center(10) + 'E_in'.center(15) + 'E_out'.center(15))
-print('-' * 40)
-for M, error_entrenamiento, error_validacion in errores_M:
-    print(
-        f'{M}'.center(10) 
-        + f'{error_entrenamiento:.2f}'.center(15) 
-        + f'{error_validacion:.2f}'.center(15)
-    )
-print('-' * 40 + '\n')
-
-### Para diferentes cantidades de variables seleccionadas ###
-print("Errores a diferentes cantidades de variables seleccionadas\n")
-errores_var = []
-for vs in [2, 4, 8, 12, 16, 20, 24]:
+def test_vs(datos_e, datos_v, target, clase_default, vs):
     bosque = ba.entrenar_bosque(
-        datos_entrenamiento, 
+        datos_e, 
         target,
         clase_default,
         max_profundidad=None,
@@ -137,29 +146,15 @@ for vs in [2, 4, 8, 12, 16, 20, 24]:
         variables_seleccionadas=vs
     )
 
-    predicciones_in = ba.predecir_bosque(bosque, datos_entrenamiento)
-    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_entrenamiento) if p == d[target])
-    e_in = 1.0 - (aciertos_in / len(datos_entrenamiento))
+    predicciones_in = ba.predecir_bosque(bosque, datos_e)
+    aciertos_in = sum(1 for p, d in zip(predicciones_in, datos_e) if p == d[target])
+    e_in = 1.0 - (aciertos_in / len(datos_e))
 
-    predicciones_out = ba.predecir_bosque(bosque, datos_validacion)
-    aciertos_out = sum(1 for p, d in zip(predicciones_out, datos_validacion) if p == d[target])
-    e_out = 1.0 - (aciertos_out / len(datos_validacion))
+    predicciones_out = ba.predecir_bosque(bosque, datos_v)
+    aciertos_out = sum(1 for p, d in zip(predicciones_out, datos_v) if p == d[target])
+    e_out = 1.0 - (aciertos_out / len(datos_v))
 
-    errores_var.append( (vs, e_in, e_out) )
+    return (vs, e_in, e_out)
 
-# Errores
-print('vars'.center(10) + 'E_in'.center(15) + 'E_out'.center(15))
-print('-' * 40)
-for vs, error_entrenamiento, error_validacion in errores_var:
-    print(
-        f'{vs}'.center(10) 
-        + f'{error_entrenamiento:.2f}'.center(15) 
-        + f'{error_validacion:.2f}'.center(15)
-    )
-print('-' * 40 + '\n')
-
-# Entrena con la mejor profundidad
-#arbol = an.entrena_arbol(datos, target, atributos, max_profundidad=3)
-#error = an.evalua_arbol(arbol, datos_entrenamiento, target)
-#print(f'Error del modelo seleccionado entrenado con TODOS los datos: {error:.2f}')
-#an.imprime_arbol(arbol)
+if __name__ == '__main__':
+    main()
